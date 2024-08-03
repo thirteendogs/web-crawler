ac271e6814d98e070bcd95c869973c8f839afe3d
thirteendogs <daniel.nova.girbau@gmail.com>
2024-08-03 23:16:16 +0200

    feat: return the urls from anchors tag

--- crawl.js
+++ crawl.js
@@ -1,3 +1,14 @@
+import { JSDOM } from "jsdom";
+
+function getURLsFromHtml(htmlBody, baseURL) {
+  const urls = [];
+  const dom = new JSDOM(htmlBody);
+  const anchors = dom.window.document.querySelectorAll("a");
+
+  anchors.forEach((anchor) => urls.push(anchor.href));
+
+  return urls;
+}
 function normalizeURL(url) {
   const myUrl = new URL(url);
 
@@ -8,4 +19,4 @@ function normalizeURL(url) {
   return `${myUrl.hostname}${myUrl.pathname}`;
 }
 
-export { normalizeURL };
+export { normalizeURL, getURLsFromHtml };
--- crawl.test.js
+++ crawl.test.js
@@ -1,16 +1,16 @@
 import { test, describe, expect } from "@jest/globals";
-import { normalizeURL } from "./crawl.js";
-
-const urls = [
-  "https://blog.boot.dev/path/",
-  "https://blog.boot.dev/path",
-  "http://blog.boot.dev/path/",
-  "http://blog.boot.dev/path",
-  "http://BLOG.boot.dev/path/",
-];
-const normalizedURL = "blog.boot.dev/path";
+import { normalizeURL, getURLsFromHtml } from "./crawl.js";
 
 describe("normalizeURL", () => {
+  const urls = [
+    "https://blog.boot.dev/path/",
+    "https://blog.boot.dev/path",
+    "http://blog.boot.dev/path/",
+    "http://blog.boot.dev/path",
+    "http://BLOG.boot.dev/path/",
+  ];
+  const normalizedURL = "blog.boot.dev/path";
+
   test("should return the normalized url", () => {
     for (const url of urls) {
       expect(normalizeURL(url)).toEqual(normalizedURL);
@@ -20,5 +20,21 @@ describe("normalizeURL", () => {
     for (const url of urls) {
       expect(normalizeURL(url).endsWith("/")).toBeFalsy();
     }
+  });
+});
+
+describe("getURLsFromHtml", () => {
+  const htmlBody = `
+    <html>
+      <body>
+        <a href="https://blog.boot.dev/">Frequently Asked Questions</a>
+      </body>
+    </html>
+`;
+  const baseUrl = "https://blog.boot.dev/";
+  test("should return an array of urls", () => {
+    expect(getURLsFromHtml(htmlBody, baseUrl)).toEqual([
+      "https://blog.boot.dev/",
+    ]);
   });
 });
